b78eb1bede528baf7ab89ab2baf976354eb813dd
yuriipereverziev <yuripereverzev@gmail.com>
2025-12-26 13:50:49 +0200

    fix crop img

--- tasks/localization.js
+++ tasks/localization.js
@@ -1,11 +1,12 @@
 const { src, dest } = require('gulp');
 const { paths } = require('./settings');
-const include = require('gulp-include');
+const browserSync = require('browser-sync');
 
 function localization() {
-  return src(paths.src.localization)
-    .pipe(include())
-    .pipe(dest(paths.build.localization));
+  // Используем glob pattern для выбора всех JSON файлов из папки
+  return src(`${paths.src.localization}/*.json`)
+    .pipe(dest(paths.build.localization))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 module.exports = localization;
